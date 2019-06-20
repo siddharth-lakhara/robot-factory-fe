c787613eae6648f7e33772094b1c069b8039cea6
lakhara <siddharth.lakhara@gmail.com>
2019-06-20 13:07:25 +0530

    add mock db

---
+++ src/utils/db.js
@@ -0,0 +1,45 @@
+import uuid from 'uuid/v4';
+
+const db = [{
+  id: uuid(),
+  name: 'Robo1',
+}, {
+  id: uuid(),
+  name: 'Robo2',
+}, {
+  id: uuid(),
+  name: 'Robo3',
+}, {
+  id: uuid(),
+  name: 'Robo4',
+}, {
+  id: uuid(),
+  name: 'Robo5',
+}, {
+  id: uuid(),
+  name: 'Robo6',
+}, {
+  id: uuid(),
+  name: 'Robo7',
+}, {
+  id: uuid(),
+  name: 'Robo8',
+}, {
+  id: uuid(),
+  name: 'Robo9',
+}, {
+  id: uuid(),
+  name: 'Robo10',
+}, {
+  id: uuid(),
+  name: 'Robo11',
+}, {
+  id: uuid(),
+  name: 'Robo12',
+}, {
+  
+  id: uuid(),
+  name: 'Robo13',
+}];
+
+export default db;
