9ebc32e1bb4fe8969bfb45803af27cd8dedbe75f
lakhara <siddharth.lakhara@gmail.com>
2019-06-20 11:54:32 +0530

    basic app setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Robo Factory</div>
+      <div>Robots list</div>
+    </div>
+  );
+}
+
+export default App;
