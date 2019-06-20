d0350d2675301411db1c3be11ff42e1f336d8719
lakhara <siddharth.lakhara@gmail.com>
2019-06-20 16:50:36 +0530

    add 404 page

--- src/components/NoMatch/NoMatch.js
+++ src/components/NoMatch/NoMatch.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import './NoMatch.css';
 
 export default class NoMatch extends Component {
   render() {
     return (
-      <div>
-        NoMatch component
+      <div className="nomatch">
+        404 : Route Not Found
       </div>
     )
   }
